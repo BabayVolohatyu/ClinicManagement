597a271c7d976327f88aaa1568298f7618f3a32d
Babay <babayvolohatyu@gmail.com>
2025-08-13 15:39:01 +0300

    Add sequential creation of dbsets

--- ClinicManagement/Data/ClinicDbContext.cs
+++ ClinicManagement/Data/ClinicDbContext.cs
@@ -1,125 +1,48 @@
 ﻿using ClinicManagement.Models.Facilities;
-using ClinicManagement.Models.People;
+using ClinicManagement.Models.Humans;
 using ClinicManagement.Models.Health;
 using Microsoft.EntityFrameworkCore;
 using ClinicManagement.Models.Info;
-using ClinicManagement.Models.People;
+using ClinicManagement.Models.Humans;
 
 namespace ClinicManagement.Data
 {
     public class ClinicDbContext : DbContext
     {
         public ClinicDbContext(DbContextOptions<ClinicDbContext> options) : base(options) { }
+        
+        //Simple models
         public DbSet<Person> People { get; set; }
+        public DbSet<Specialty> Specialties { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<CabinetType> CabinetTypes { get; set; }
+        public DbSet<Symptom> Symptoms { get; set; }
+        public DbSet<Sickness> Sicknesses { get; set; }
+        public DbSet<Treatment> Treatments { get; set; }
+        public DbSet<Procedure> Procedures { get; set; }
+
+        //Models that depend only on previous ones
         public DbSet<Patient> Patients { get; set; }
-        public DbSet<Sickness> Sicknesses {get; set; }
-        public DbSet<Symptom> Symptoms {get; set; }
-        public DbSet<SicknessSymptom> SicknessSymptoms {get; set; }
-        public DbSet<Treatment> Treatments {get; set; }
-        public DbSet<SicknessTreatment> SicknessTreatments {get; set; }
-        public DbSet<Doctor> Doctors {get; set; }
-        public DbSet<Specialty> Specialties {get; set; }
-        public DbSet<Address> Addresses {get; set; }
-        public DbSet<Cabinet> Cabinets {get; set; }
-        public DbSet<CabinetType> CabinetTypes {get; set; }
-        public DbSet<Schedule> Schedules {get; set; }
-        public DbSet<DistrictDoctor> DistrictDoctors {get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Cabinet> Cabinets { get; set; }
+        public DbSet<SicknessSymptom> SicknessSymptoms { get; set; }
+        public DbSet<SicknessTreatment> SicknessTreatments { get; set; }
+        public DbSet<SicknessProcedure> SicknessProcedures { get; set; }
+
+        //Models that depend only on previous ones(third wave)
+        public DbSet<DistrictDoctor> DistrictDoctors { get; set; }
+        public DbSet<DoctorOnCallStatus> DoctorOnCallStatuses { get; set; }
+        public DbSet<DoctorProcedure> DoctorProcedures { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+
+        //Models that depend only on previous ones(fourth wave)
+        public DbSet<Appointment> Appointments { get; set; }
+
+        //Models that depend only on previous ones(fifth wave)
+        public DbSet<Diagnosis> Diagnoses { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {   
-            //One-to-one from Patient to Person
-            modelBuilder.Entity<Patient>()
-                .HasOne(pt => pt.Person)
-                .WithOne(p => p.Patient)
-                .HasForeignKey<Patient>(pt => pt.PersonId);
+        {
 
-            //SicknessSymptom
-            modelBuilder.Entity<SicknessSymptom>()
-                .HasKey(ss => new { ss.SicknessId, ss.SymptomId });
-
-            //Sickness-Symptom many-to-many relationship
-            //One to many from SS to Sickness  
-            modelBuilder.Entity<SicknessSymptom>()
-                .HasOne(ss => ss.Sickness)
-                .WithMany(s => s.SicknessSymptoms)
-                .HasForeignKey(ss => ss.SicknessId);
-
-            //One to many from SS to Symptom
-            modelBuilder.Entity<SicknessSymptom>()
-                .HasOne(ss => ss.Symptom)
-                .WithMany(s => s.SicknessSymptoms)
-                .HasForeignKey(ss => ss.SymptomId);
-
-            //SicknessTreatment
-            modelBuilder.Entity<SicknessTreatment>()
-                .HasKey(st => new { st.SicknessId, st.TreatmentId });
-
-            //Sickness-Treatment many-to-many relationship
-            //One-to-many from ST to Sickness
-            modelBuilder.Entity<SicknessTreatment>()
-                .HasOne(st => st.Sickness)
-                .WithMany(t => t.SicknessTreatment)
-                .HasForeignKey(st => st.SicknessId);
-
-            //One-to-many from ST to Treatment
-            modelBuilder.Entity<SicknessTreatment>()
-                .HasOne(st => st.Treatment)
-                .WithMany(s => s.SicknessTreatment)
-                .HasForeignKey(st => st.TreatmentId);
-
-            //One-to-one from Doctor to Person
-            modelBuilder.Entity<Doctor>()
-                .HasOne(d => d.Person)
-                .WithOne(p => p.Doctor)
-                .HasForeignKey<Doctor>(d => d.PersonId);
-
-            //DistrictDoctor
-            modelBuilder.Entity<DistrictDoctor>()
-                .HasKey(dd =>  dd.DoctorId);
-            //One-to-one from DD to Doctor
-            modelBuilder.Entity<DistrictDoctor>()
-                .HasOne(dd => dd.Doctor)
-                .WithOne(d => d.DistrictDoctor)
-                .HasForeignKey<DistrictDoctor>(dd => dd.DoctorId);
-       
-
-            //One-to-many from Specialty to Doctor
-            modelBuilder.Entity<Specialty>()
-                .HasMany(s => s.Doctors)
-                .WithOne(d => d.Specialty)
-                .HasForeignKey(d => d.SpecialtyId)
-                .IsRequired();
-
-            //One-to-many from CT to Cabinet
-            modelBuilder.Entity<CabinetType>()
-                .HasMany(ct => ct.Cabinets)
-                .WithOne(c => c.Type)
-                .HasForeignKey(c => c.TypeId)
-                .IsRequired();
-
-
-            //Explicit definition of Schedule date type
-            modelBuilder.Entity<Schedule>()
-                .Property(s => s.StartTime)
-                .HasColumnType("timestamp")
-                .IsRequired();
-
-            modelBuilder.Entity<Schedule>()
-                .Property(s => s.EndTime)
-                .HasColumnType("timestamp")
-                .IsRequired();
-
-            //One-to-many from Doctor to Schedule
-            modelBuilder.Entity<Doctor>()
-                .HasMany(d => d.Schedules)
-                .WithOne(s => s.Doctor)
-                .HasForeignKey(s => s.DoctorId)
-                .IsRequired();
-            //One-to-many from Cabinet to Schedule
-            modelBuilder.Entity<Cabinet>()
-               .HasMany(c => c.Schedules)
-               .WithOne(s => s.Cabinet)
-               .HasForeignKey(s => s.CabinetId)
-               .IsRequired();
         }
     }
 }
