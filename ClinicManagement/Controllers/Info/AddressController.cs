86233b21534a23529a87d8d1ddb7da2da036be5e
Babay <babayvolohatyu@gmail.com>
2025-08-18 19:12:45 +0300

    Remove create get/post methods to the generic controller

--- ClinicManagement/Controllers/Info/AddressController.cs
+++ ClinicManagement/Controllers/Info/AddressController.cs
@@ -1,25 +1,10 @@
 ﻿using ClinicManagement.Data;
 using ClinicManagement.Models.Info;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicManagement.Controllers.Info
 {
     public class AddressController : GenericController<Address>
     {
         public AddressController(ClinicDbContext context) : base(context) { }
-
-        [HttpGet("create")]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost("create")]
-        public async Task<IActionResult> Create(Address address)
-        {
-            _context.Addresses.Add(address);
-             await _context.SaveChangesAsync();
-             return RedirectToAction(nameof(Index));
-        }
     }
 }
