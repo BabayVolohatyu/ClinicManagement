e057d1932290842e66e085a75a05221bf4bfdd4a
Babay <babayvolohatyu@gmail.com>
2025-09-06 17:08:48 +0300

    Add validation

--- ClinicManagement/Controllers/Humans/PatientController.cs
+++ ClinicManagement/Controllers/Humans/PatientController.cs
@@ -13,8 +13,11 @@ namespace ClinicManagement.Controllers.Humans
         [HttpGet]
         public override IActionResult Create()
         {
+            var availablePeople = _context.People
+                .Where(p => p.Patient == null)                               
+                .ToList();
 
-            ViewBag.PersonId = new SelectList(_context.People, "Id", "Id");
+            ViewBag.PersonId = new SelectList(availablePeople, "Id", "Id");
             ViewBag.AddressId = new SelectList(_context.Addresses, "Id", "Id");
             return View();
         }
