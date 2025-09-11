4fac568bdc8446df64bae76e05beb06e978e0a94
Babay <babayvolohatyu@gmail.com>
2025-09-11 12:42:35 +0300

    Implement person controller

--- ClinicManagement/Controllers/Humans/PersonController.cs
+++ ClinicManagement/Controllers/Humans/PersonController.cs
@@ -1,6 +1,8 @@
 ﻿using ClinicManagement.Data;
 using ClinicManagement.Models.Humans;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace ClinicManagement.Controllers.Humans
 {
@@ -8,16 +10,62 @@ namespace ClinicManagement.Controllers.Humans
     {
         private readonly ClinicDbContext _context;
 
+        private const string _route = "Views/Humans/Person/";
         public PersonController(ClinicDbContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var people = _context.People.ToList();
-            return View("Views/Humans/Person/Index.cshtml", people);
+            var people = await _context.People.ToListAsync();
+
+            return View(ViewPath("Index"), people);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var person = await _context.People.FindAsync(id);
+            return View(ViewPath("Details"), person);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Person person)
+        {
+            _context.People.Update(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var person = await _context.People.FindAsync(id);
+            if (person != null)
+            {
+                _context.People.Remove(person);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(ViewPath("Create"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Person person)
+        {
+            await _context.People.AddAsync(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private string ViewPath(string viewName) => _route + viewName + ".cshtml";
+
     }
 }
