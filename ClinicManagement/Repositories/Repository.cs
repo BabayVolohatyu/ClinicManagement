ec6b42c136f04eb9c1a29ffd540c80e535055fbe
Babay <babayvolohatyu@gmail.com>
2025-11-06 19:41:25 +0200

    Rewrite class to be abstract

--- ClinicManagement/Repositories/Repository.cs
+++ ClinicManagement/Repositories/Repository.cs
@@ -4,7 +4,7 @@ using System.Linq.Expressions;
 
 namespace ClinicManagement.Repositories
 {
-    public class Repository<T> : IRepository<T> where T : class
+    public abstract class Repository<T> : IRepository<T> where T : class
     {
         protected readonly ClinicDbContext _context;
         protected readonly DbSet<T> _dbSet; 
@@ -15,39 +15,37 @@ namespace ClinicManagement.Repositories
             _dbSet = _context.Set<T>();
         }
 
-        public async Task AddAsync(T entity)
+        public virtual async Task AddAsync(T entity)
         {
             await _dbSet.AddAsync(entity);
         }
 
-        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
         {
            return await _dbSet
                 .Where(predicate)
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<T>> GetAllAsync(int pageNumber = 1, int pageSize = 10)
+        public virtual async Task<IEnumerable<T>> GetAllAsync(int pageNumber = 1, int pageSize = 10)
         {
-            if(pageNumber < 1) pageNumber = 1;
-            if(pageSize < 1) pageSize = 1;
             return await _dbSet
                 .Skip((pageNumber-1)*pageSize)
                 .Take(pageSize)
                 .ToListAsync();
         }
 
-        public async Task<T?> GetByIdAsync(int id) =>
+        public virtual async Task<T?> GetByIdAsync(int id) =>
             await _dbSet.FindAsync(id);
 
-        public void Remove(T entity)
+        public virtual void Remove(T entity)
         {
             _dbSet.Remove(entity);
         }
 
-        public async Task SaveChangesAsync() =>
+        public virtual async Task SaveChangesAsync() =>
             await _context.SaveChangesAsync();
-        public void Update(T entity)
+        public virtual void Update(T entity)
         {
             _dbSet.Update(entity);
         }
