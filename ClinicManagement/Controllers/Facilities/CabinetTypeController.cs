45518af64599516400835bd60b8de9255e1d90f4
Babay <babayvolohatyu@gmail.com>
2025-11-08 15:44:04 +0200

    Add inheritanve of the CommonController

--- ClinicManagement/Controllers/Facilities/CabinetTypeController.cs
+++ ClinicManagement/Controllers/Facilities/CabinetTypeController.cs
@@ -1,19 +1,14 @@
-﻿using ClinicManagement.Data;
+﻿using ClinicManagement.Controllers.Base;
+using ClinicManagement.Data;
+using ClinicManagement.Models.Facilities;
+using ClinicManagement.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicManagement.Controllers.Facilities
 {
-    public class CabinetTypeController : Controller
+    public class CabinetTypeController : CommonController<CabinetType>
     {
-        private readonly ClinicDbContext _context;
-
-        public CabinetTypeController(ClinicDbContext context)
-        {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public CabinetTypeController(IService<CabinetType> service, ILogger<CabinetTypeController> logger)
+            : base(service, logger) { }
     }
 }
