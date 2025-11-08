ce666e0839c0759efd485506ce94fd2079f82ed2
Babay <babayvolohatyu@gmail.com>
2025-11-08 20:45:19 +0200

    Enhance the program.cs

--- ClinicManagement/Program.cs
+++ ClinicManagement/Program.cs
@@ -1,20 +1,35 @@
 using ClinicManagement.Data;
+using ClinicManagement.Models.Facilities;
+using ClinicManagement.Services;
+using ClinicManagement.Services.Facilities;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
 //Add connection to the PostgreSQL DB
 builder.Services.AddDbContext<ClinicDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IService<CabinetType>, CabinetTypeService>();
 
 //Add global model validator
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add<ValidateModelFilter>();
 });
+
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.ViewLocationFormats.Clear();
+
+    options.ViewLocationFormats.Add("/Views/Facilities/{1}/{0}.cshtml");
+    options.ViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+});
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
