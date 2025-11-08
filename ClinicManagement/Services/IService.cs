fcfa172db8feacb4579b5a4d0136ade41f378ba9
Babay <babayvolohatyu@gmail.com>
2025-11-08 14:16:25 +0200

    Merge Repos with Service layer. Erase Repo layer

--- ClinicManagement/Services/IService.cs
+++ ClinicManagement/Services/IService.cs
@@ -1,13 +1,12 @@
 ﻿using System.Linq.Expressions;
 
-namespace ClinicManagement.Repositories
+namespace ClinicManagement.Services
 {
-    public interface IRepository<T> where T : class
+    public interface IService<T> where T : class
     {
         Task AddAsync(T entity);
-        void Update(T entity);
-        void Remove(T entity);
-        Task SaveChangesAsync();
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
         Task<T?> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync(int pageNumber = 1, int pageSize = 10);
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
---
+++ ClinicManagement/Services/Service.cs
@@ -0,0 +1,68 @@
+﻿using ClinicManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ClinicManagement.Services
+{
+    public abstract class Service<T> : IService<T> where T : class
+    {
+        protected readonly ClinicDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        protected Service(ClinicDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public virtual async Task AddAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet
+                .Where(predicate)
+                .ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync(int pageNumber = 1, int pageSize = 10)
+        {
+            if (pageNumber < 1) pageNumber = 1;
+            if (pageSize < 1 || pageSize > 100) pageSize = 10;
+
+            return await _dbSet
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public virtual async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task RemoveAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
