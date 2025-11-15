789257ce4ba720efb53aa167c98c268c9c9ae1ef
Babay <babayvolohatyu@gmail.com>
2025-11-15 15:29:56 +0200

    Add reg/log model

---
+++ ClinicManagement/Models/Auth/RegisterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicManagement.Models.Auth
+{
+    public class RegisterModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
